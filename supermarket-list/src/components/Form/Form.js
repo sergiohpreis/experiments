9d1ff81c4da5f1e0b0b8a3f960c2a060ba1c074a
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-09-08 15:52:34 -0300

    supermarket-list | Adding shortcut

--- supermarket-list/src/components/Form/Form.js
+++ supermarket-list/src/components/Form/Form.js
@@ -5,19 +5,27 @@ import Button from '../Button/Button';
 
 import './Form.css';
 
-const Form = props => (
-    <div className="form">
-        <h3 className="form__title">{props.title}</h3>
-        <div className="form__container form__container--product">
-            <Input title="Produto" placeholder="Qual o produto ?" value={props.description} onChange={props.handleDescription}/>
+const Form = props => {
+    const shortcuts = e => {
+        if (e.key === 'Enter') {
+            props.handleInclude();
+        };
+    };
+
+    return (
+        <div className="form">
+            <h3 className="form__title">{props.title}</h3>
+            <div className="form__container form__container--product">
+                <Input title="Produto" placeholder="Qual o produto ?" value={props.description} onChange={props.handleDescription} onKeyUp={shortcuts}/>
+            </div>
+            <div className="form__container form__container--qtd">
+                <Input title="Qtd" placeholder="Qual a quantidade ?" value={props.qtd} onChange={props.handleQtd} onKeyUp={shortcuts}/>
+            </div>
+            <div className="form__button">
+                <Button type="add" text="Include" onClick={props.handleInclude}/>
+            </div>
         </div>
-        <div className="form__container form__container--qtd">
-            <Input title="Qtd" placeholder="Qual a quantidade ?" value={props.qtd} onChange={props.handleQtd}/>
-        </div>
-        <div className="form__button">
-            <Button type="add" text="Include" onClick={props.handleInclude}/>
-        </div>
-    </div>
-);
+    );
+}
 
 export default Form;
