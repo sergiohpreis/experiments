d5c7bf0d7a0f66956ea624e8ca628594cba6f597
Sergio Henrique Pereira Reis <sergio.hpreis@gmail.com>
2017-09-08 14:03:38 -0300

    supermarket-list | Creating Icon Component

--- supermarket-list/src/main/App.js
+++ supermarket-list/src/main/App.js
@@ -3,11 +3,14 @@ import React from 'react';
 import './App.css';
 
 import Input from '../components/Input/Input';
+import Icon from '../components/Icon/Icon';
 
 
 const App = props => (
     <div className="App">
         <Input title="Item" placeholder="Escreva o nome do item"/>
+        <Icon type="add" text="Add"/>
+        <Icon type="remove" text="Remove"/>
     </div>
 );
 
